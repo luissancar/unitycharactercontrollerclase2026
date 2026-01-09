61cd03241f6b9715c8289dd9c241366fa2ccff83
cesaruegh25 <csanmal2502@g.educaand.es>
2026-01-09 10:22:53 +0100

    animaciones
    se han añadido animaciones idle, caminar en todas direcciones

---
+++ Assets/Juego3D/Scripts/animation/animation.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class animation : MonoBehaviour
+{
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private CharacterController characterController;
+
+    [Tooltip("velocidad maxima utilizada para normalizar el movimiento")]
+    private float velocidadMax = 1f;
+
+    private Vector3 movimientoLocal;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (characterController == null)
+            characterController = GetComponent<CharacterController>();
+        if (animator == null)
+            animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ActualizarMovimiento();
+
+
+    }
+
+    private void ActualizarMovimiento()
+    {
+        Vector3 velocidad = characterController.velocity;
+        movimientoLocal = transform.InverseTransformDirection(velocidad);
+        float x = movimientoLocal.x;
+        float y = movimientoLocal.z;
+
+        if (velocidadMax > 0)
+        {
+            x /= velocidadMax;
+            y /= velocidadMax;
+        }
+
+        animator.SetFloat("X", x);
+        animator.SetFloat("Y", y);
+        animator.SetBool("suelo", characterController.isGrounded);
+
+    }
+
+}
